4d46fa5f866d78fb55b028daceaaf315dde0e936
Зайцева Дарья <errorka792@gmail.com>
2018-09-08 19:00:18 +0300

    changes randomNum formula

--- js/goods.js
+++ js/goods.js
@@ -10,7 +10,7 @@ var PICTURE_PATH = 'img/cards/';
 
 
 var getRandomNum = function (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 var getRandomInd = function (array) {
   return Math.floor(Math.random() * array.length);
