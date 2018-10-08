37351f7aa3259e4084eb3c4620f14a92e803c004
Зайцева Дарья <errorka792@gmail.com>
2018-10-08 22:43:46 +0300

    wip

--- js/filter.js
+++ js/filter.js
@@ -11,66 +11,6 @@
     'Зефир': 'marshmallow'
   };
 
-  /* фильтры по категории(тип):
-  1. комбинируются между собой
-  2. комбинируются с фильтрами по составку
-  3. комбинируются с сортировкой
-  4. если не выбран ни один фильтр (или отменен), то показывается изначальный список с сервера
-
-  фильтры по составу (проперти):
-  1. комбинируются между собой
-  2. комбинируются с фильтрами по категории
-  3. комбинируются с сортировкой
-  4. если не выбран, то отменяет своё действие
-
-  фильтры только избранное и в наличии (пометки)
-  1. отменяют все остальные фильтры
-  2. отменяют друг друга (одновременно показан только один фильтр) ==> не нужна функция получения checkedvalue
-  3. комбинируются с сортировкой
-
-  фильтр по цене
-  1.  Нижняя и верхняя границы могут совпадать, в этом случае в список должны попадать только те товары,
-   цена которых чётко соответствует указанной в фильтре.
-  2. при изменении цены, меняется кол-во
-
-  индикатор кол-ва
-  1. есть начальные значения у всех пунктов
-  2. при изменении фильтров категории и фильтров состава, у ЦЕНЫ должно меняться кол-во
-  3. при смещении цены, у цены меняется кол-во
-  4. при добавлении в избранное меняется кол-во
-  5. "в наличие" всегда равно изначальному кол-ву товаров
-  4. должно ли меняться кол-во при категория + состав? в ТЗ нет, но логично так сделать
-
-
-  сначала популярные: товары в исходном порядке, в котором они были загружены с сервера;
-  сначала дорогие;
-  сначала дешёвые;
-  по рейтингу: товары, расположенные в порядке убывания рейтинга.
-  При совпадении рейтинга, выше показывается товар, у которого больше голосов в рейтинге.
-
-   «Показать всё» сбрасывает все выбранные фильтры и показывает список в исходном состоянии.
-
-
-   ВЫВОДЫ:
-   1. нужна начальная настройка =  фильтры и сортировка сброшены, индикаторы кол-ва, ползунок цены на 0 - 90
-   2. кобинируются между собой:
-
-      категория + категория,
-      категория + сортировка
-
-      состав + состав,
-      состав + сортировка
-
-      категория + состав,
-      категория + состав + сортировка
-
-      избранное + сортировка
-      в наличии + сортировка
-
-    4. показать все, в наличии = показывают начальный список?
-       по популярности запоминает позицию карточек от начальной выгрузки с сервера?
-       по рейтингу комбинирует сортировку по голосам и звездочкам?
-   */
 
   var setDefaultParams = function () {
     Array.from(document.querySelectorAll('input[name="food-type"], input[name="food-property"]')).forEach(function (elem) {
@@ -275,83 +215,108 @@
 
       window.filter.filteredCards = filteredCards;
     });
-  };
 
-  // ползунок фильтра по цене
-  var LEFT = 'left';
-  var RIGHT = 'right';
-  var rangeSliderHandler = document.querySelector('.range__filter');
-  var fillLine = rangeSliderHandler.querySelector('.range__fill-line');
-  var toggle = rangeSliderHandler.querySelector('.range__btn');
+    // ползунок фильтра по цене
+    var LEFT = 'left';
+    var RIGHT = 'right';
+    var rangeSliderHandler = document.querySelector('.range__filter');
+    var fillLine = rangeSliderHandler.querySelector('.range__fill-line');
+    var toggle = rangeSliderHandler.querySelector('.range__btn');
 
-  var Toggler = {};
-  Toggler[LEFT] = rangeSliderHandler.querySelector('.range__btn--left');
-  Toggler[RIGHT] = rangeSliderHandler.querySelector('.range__btn--right');
+    var Toggler = {};
+    Toggler[LEFT] = rangeSliderHandler.querySelector('.range__btn--left');
+    Toggler[RIGHT] = rangeSliderHandler.querySelector('.range__btn--right');
 
-  var Price = {};
-  Price[LEFT] = document.querySelector('.range__price--min');
-  Price[RIGHT] = document.querySelector('.range__price--max');
-  var toggleCenter = toggle.offsetWidth / 2;
+    var Price = {};
+    Price[LEFT] = document.querySelector('.range__price--min');
+    Price[RIGHT] = document.querySelector('.range__price--max');
+    var toggleCenter = toggle.offsetWidth / 2;
 
-  // поиск мин и макс значения цены
-  var findMinAndMaxPrice = function (isMax) {
-    var prices = window.catalog.goods.map(function (good) {
-      return good.price;
-    });
-    var pricesCopy = prices.slice();
-    pricesCopy.sort(function (first, second) {
-      return first - second;
-    });
+    // поиск мин и макс значения цены
+    var findMinAndMaxPrice = function (goods, isMax) {
+      var prices = goods.map(function (good) {
+        return good.price;
+      }).sort(function (first, second) {
+        return first - second;
+      });
 
-    return isMax ? pricesCopy.pop() : pricesCopy.shift();
-  };
-
-  rangeSliderHandler.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
+      return isMax ? prices.pop() : prices.shift();
+    };
 
     var slider = {
       startPos: 0,
       endPos: rangeSliderHandler.offsetWidth - toggleCenter,
-      minPin: findMinAndMaxPrice(window.catalog.goods, false),
+      minPin: findMinAndMaxPrice(window.catalog.goods),
       maxPin: findMinAndMaxPrice(window.catalog.goods, true)
     };
 
-    var startXCoords = evt.clientX;
-
-    var onMouseMove = function (moveEvt) {
-      var shift = startXCoords - moveEvt.clientX;
-      startXCoords = moveEvt.clientX;
-
-      switch (evt.target) {
-        case Toggler[LEFT]:
-          return moveToggler(LEFT, slider.startPos, Toggler[RIGHT].offsetLeft, shift);
-        case Toggler[RIGHT]:
-          return moveToggler(RIGHT, Toggler[LEFT].offsetLeft, slider.endPos, shift);
-      }
-      return false;
+    var settingInitPrices = function () {
+      Price[LEFT].textContent = slider.minPin;
+      Price[RIGHT].textContent = slider.maxPin;
     };
+    settingInitPrices();
 
-    var moveToggler = function (side, min, max, shift) {
-      var newCoord = Toggler[side].offsetLeft - shift;
-      if (newCoord >= min && newCoord <= max) {
-        Toggler[side].style.left = newCoord + 'px';
-        fillLine.style[side] = Math.abs(newCoord - (side === RIGHT ? slider.endPos : 0)) + 'px';
-        Price[side].textContent = Math.round(newCoord / slider.endPos * slider.maxPin);
-      }
+    rangeSliderHandler.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
+
+      var startXCoords = evt.clientX;
+
+      var onMouseMove = function (moveEvt) {
+        var shift = startXCoords - moveEvt.clientX;
+        startXCoords = moveEvt.clientX;
+
+        switch (evt.target) {
+          case Toggler[LEFT]:
+            return moveToggler(LEFT, slider.startPos, Toggler[RIGHT].offsetLeft, shift);
+          case Toggler[RIGHT]:
+            return moveToggler(RIGHT, Toggler[LEFT].offsetLeft, slider.endPos, shift);
+        }
+        return false;
+      };
+
+      var moveToggler = function (side, min, max, shift) {
+        var newCoord = Toggler[side].offsetLeft - shift;
+        if (newCoord >= min && newCoord <= max) {
+          Toggler[side].style.left = newCoord + 'px';
+          fillLine.style[side] = Math.abs(newCoord - (side === RIGHT ? slider.endPos : 0)) + 'px';
+          Price[side].textContent = Math.round(newCoord / slider.endPos * slider.maxPin);
+        }
+
+        var calcCurrentMinMaxPos = function () {
+          var currentMinPos = slider.minPin;
+          var currentMaxPos = slider.maxPin;
+          if (Price[side] === LEFT) {
+            currentMinPos = Price[side];
+          }
+          if (Price[side] === RIGHT) {
+            currentMaxPos = Price[side];
+          }
+          return {
+            currentSliderMaxPos: currentMaxPos,
+            currentSliderMinPos: currentMinPos
+          };
+        };
+
+        // window.filter.filterByRangePrice = function (cards) {
+        //   return cards.filter(function (card) {
+        //     return card.price >= calcCurrentMinMaxPos().currentSliderMinPos && card.price <= calcCurrentMinMaxPos().currentSliderMaxPos;
+        //   });
+        // };
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+        // window.filter.filteredCards = window.filter.filterByRangePrice(window.filter.filteredCards);
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
+    window.filter = {
+      updateCatalog: updateCatalog,
+      updateMarkCounters: updateMarkCounters
     };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-
-  window.filter = {
-    updateCatalog: updateCatalog,
-    updateMarkCounters: updateMarkCounters
   };
 })();
